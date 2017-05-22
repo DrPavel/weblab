ebce9126af2d3ebe70adda8401babec041ffbd30
Pavel <pavellekshin@yandex.ru>
2017-05-23 01:34:32 +0300

    23.05.2017 1:30
    Приблизительная мат модель 3 лабы

--- lab3/lab3.js
+++ lab3/lab3.js
@@ -1,9 +1,25 @@
+var lab=false;
+var r=0;
+var m=0;
+var h=0;
+var q=0;
+var time=0;
+
+
 function tel_poz(){
+	if((h<100)&(lab==true)){
+		r=document.getElementById("rad").value;
+		m=document.getElementById("mass").value;
 	
-	/*
-	var a=rs*m*g/;
-	var h=h0+;
-	var q=h*360/100;*/
+		//var a=rs*m*g/1;
+		h=0.046*m*9.82*time*time/(8*0.220*r);
+		q=h*360/100;
+		block.style.top=((h)*3.97+50)+"px";
+		kol.style.transform=" rotate(-"+(q)+"deg)";
+		document.getElementById("time").innerHTML="t="+time+" с."
+	time++;
+	setTimeout(tel_poz,60);
+	}
 }
 
 function start_l(){
@@ -17,14 +33,11 @@ function stop_l(){
 
 function res_l(){
 	lab=false;
-	poz=0;
+	h=0;
 	time=0;
-	block.style.left=poz+227+"px";
-	block.style.top=poz+50+"px";
-	
-	document.getElementById("time").innerHTML="t=0 мс."
-	document.getElementById("pozition").innerHTML="x=0 мм."
-	document.getElementById("speed").innerHTML="v=0 мм./мc."
+	block.style.top=((h)*3.97+50)+"px";
+	kol.style.transform=" rotate(0deg)";
+	document.getElementById("time").innerHTML="t=0 c."
 	document.getElementById("graf_speed").innerHTML="";
 	document.getElementById("graf_poz").innerHTML="";
 }
@@ -32,16 +45,4 @@ function res_l(){
 function corner(){
 	sten.style.transform="rotate("+document.getElementById("corner").value+"deg)";
 	tel_1.style.transform="rotate("+document.getElementById("corner").value+"deg)";
-}
-
-function add_dat(){
-	document.getElementById("exp").innerHTML+="<div id='dat_"+dat+"' class='dat' style='top:"+(200-50*dat)+"px;'><input type='num' id='dat_poz_"+dat+"' onclick='pozit();'><br /><input type='num' id='dat_data_"+dat+"'></div>";
-	dat++;
-}
-
-function pozit(n){
-	corner=document.getElementById("corner").value*Math.PI/180;
-	x=document.getElementById("dat_poz_0").value;
-	dat_0.style.top=x*Math.sin(corner)+200+"px";
-	dat_0.style.left=x*Math.cos(corner)+10+"px";
 }
